16f3846cbb36e61bbef2408e7392405219c5b7d7
Hồ Thanh Nhàn <thanhnhan11012005@gmail.com>
2026-06-19 08:14:15 +0700

    feat: add docker and vercel

--- BE/src/main/java/com/smartspender/config/WebSecurityConfig.java
+++ BE/src/main/java/com/smartspender/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
